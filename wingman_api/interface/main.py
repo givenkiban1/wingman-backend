c38b2ffa8e8919c4d7dd07fbd5e2d47ceb627917
Given <gkibanzait@gmail.com>
2023-07-06 11:47:07 +0200

    predictions failing because of Error: Feature names seen at fit time, yet now missing:

--- wingman_api/interface/main.py
+++ wingman_api/interface/main.py
@@ -22,6 +22,64 @@ from wingman_api.ml_logic.preprocessor import preprocess_features
 from wingman_api.ml_logic.registry import load_model, save_model, save_results
 from wingman_api.ml_logic.registry import mlflow_run, mlflow_transition_model
 
+PREPROCESSED_FIELDS = ['num_eng',
+ 'total_seats',
+ 'afm_hrs',
+ 'cert_max_gr_wt',
+ 'dprt_time',
+ 'power_units',
+ 'flight_hours_mean',
+ 'certs_held',
+ 'second_pilot',
+ 'site_seeing',
+ 'air_medical',
+ 'crew_sex',
+ 'type_last_insp_100H',
+ 'type_last_insp_AAIP',
+ 'type_last_insp_ANNL',
+ 'type_last_insp_COAW',
+ 'type_last_insp_COND',
+ 'type_last_insp_UNK',
+ '_AAPL',
+ '_AOBV',
+ '_BUS ',
+ '_FLTS',
+ '_INST',
+ '_OWRK',
+ '_Other',
+ '_PERS',
+ '_POSI',
+ '_UNK',
+ 'eng_mfgr_CONTINENTAL',
+ 'eng_mfgr_LYCOMING',
+ 'eng_mfgr_P&W',
+ 'eng_mfgr_ROTAX',
+ 'eng_mfgr_infrequent_sklearn',
+ 'far_part_091',
+ 'far_part_infrequent_sklearn',
+ 'acft_make_BEECH',
+ 'acft_make_CESSNA',
+ 'acft_make_PIPER',
+ 'acft_make_infrequent_sklearn',
+ 'fixed_retractable_RETR',
+ 'acft_category_AIR',
+ 'acft_category_infrequent_sklearn',
+ 'homebuilt',
+ 'crew_category_DSTU',
+ 'crew_category_FLTI',
+ 'crew_category_PLT',
+ 'eng_type_REC',
+ 'eng_type_infrequent_sklearn',
+ 'carb_fuel_injection_CARB',
+ 'carb_fuel_injection_FINJ',
+ 'carb_fuel_injection_UNK',
+ 'dprt_apt_id',
+ 'dest_apt_id',
+ 'flt_plan_filed_IFR',
+ 'flt_plan_filed_NONE',
+ 'flt_plan_filed_VFR',
+ 'pc_profession']
+
 
 def pred(X_pred: pd.DataFrame = None) -> np.ndarray:
     """
@@ -44,6 +102,14 @@ def pred(X_pred: pd.DataFrame = None) -> np.ndarray:
     assert model is not None
     X_clean = clean_data(X_pred)
     X_processed = preprocess_features(X_clean, target_columns=[])
+
+    values = [0 for field in PREPROCESSED_FIELDS]
+
+    df = pd.DataFrame(values, columns=PREPROCESSED_FIELDS)
+
+    df[X_processed.columns.to_list()] = X_processed
+
+    print(X_processed.columns.to_list())
     y_pred = model.predict(X_processed)
 
     print("\n✅ prediction done: ", y_pred, y_pred.shape, "\n")
